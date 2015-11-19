f36f1929fe5939aecb312853fc8a83452d619742
Marcus Kertesz <ModernMAK@Hotmail.com>
2015-11-19 09:58:42 -0800

    Update QrCodeWrapper.cs

--- QrCodeWrapper.cs
+++ QrCodeWrapper.cs
@@ -1,7 +1,5 @@
 using UnityEngine;
 
-//ZXing is a library, this library is the C# port, link later
-
 using ZXing;
 using ZXing.QrCode;
 using ZXing.Common;
@@ -10,11 +8,14 @@ using ZXing.QrCode.Internal;
 public class QrCodeWrapper {
 	
 	private static 
-		QRCodeReader codeReader = new QRCodeReader();
+		QRCodeReader CodeReader = new QRCodeReader();
+
 	private static 
-		QRCodeWriter codeWriter = new QRCodeWriter();
+		QRCodeWriter CodeWriter = new QRCodeWriter();
+
 	private static
-		Color32Renderer colRender = new Color32Renderer();
+		Color32Renderer ColorRenderer = new Color32Renderer();
+
 	public const int 
 		QR_DIMENSION = 256;
 
@@ -28,20 +29,21 @@ public class QrCodeWrapper {
 	
 	public static Result DecodeImage(Texture2D tex)
 	{
-
 		Color32LuminanceSource colLumSource = new Color32LuminanceSource(tex.GetPixels32(), tex.width, tex.height);
 		HybridBinarizer hybridBinarizer = new HybridBinarizer(colLumSource);
 		BinaryBitmap bitMap = new BinaryBitmap(hybridBinarizer);
 		//We reset before we decode for safety;
-		codeReader.reset();
 
-		return codeReader.decode(bitMap);
+
+		CodeReader.reset();
+
+		return CodeReader.decode(bitMap);
 	}
 	
 	public static Texture2D EncodeImage(string data)
 	{
-		BitMatrix bitMat = codeWriter.encode(data, BarcodeFormat.QR_CODE, QR_DIMENSION, QR_DIMENSION);
-		Color32[] cols = colRender.Render(bitMat, BarcodeFormat.QR_CODE, "");
+		BitMatrix bitMat = CodeWriter.encode(data, BarcodeFormat.QR_CODE, QR_DIMENSION, QR_DIMENSION);
+		Color32[] cols = ColorRenderer.Render(bitMat, BarcodeFormat.QR_CODE,"");
 		Texture2D tex = new Texture2D(bitMat.Width,bitMat.Height);
 		//we set pixels to the new image
 		tex.SetPixels32(cols);
