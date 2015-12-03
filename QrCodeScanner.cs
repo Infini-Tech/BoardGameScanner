ad8439f4fa80f8f6a201b90e03efae37f65858b0
Marcus Kertesz <ModernMAK@Hotmail.com>
2015-12-03 09:54:30 -0800

    Update QrCodeScanner.cs
    Removed Debug. Modified so that Camera only runs when scanning.

--- QrCodeScanner.cs
+++ QrCodeScanner.cs
@@ -3,8 +3,6 @@ using System.Collections;
 using UnityEngine.UI;
 
 public class QrCodeScanner : MonoBehaviour {
-	//Remove Debug Info
-
 	private WebcamWrapper 
 		myWrapper;
 
@@ -15,8 +13,7 @@ public class QrCodeScanner : MonoBehaviour {
 		myStartSearch;
 
 	public Text
-		myText,
-		myDebug;
+		myText;
 
 	private string 
 		myContent;
@@ -28,72 +25,55 @@ public class QrCodeScanner : MonoBehaviour {
 		myWrapper = new WebcamWrapper ();
 		myWrapper.StartCamera ();
 		myWrapper.Paint (myCameraPlane);
+		myWrapper.StopCamera ();
 		isScanning = false;
 		myStartSearch.isOn = false;
-		myDebug.text = "Awake";
-
-
 	}
 	public virtual void Update(){
 		if (!isScanning && myStartSearch.isOn) {
-			myDebug.text = "Start Scan";
 			StartCoroutine(Scan ());
 		}
 		if (isScanning && !myStartSearch.isOn) {
-			myDebug.text = "Fixing Dirty Scan";
 			isScanning = false;
-			myDebug.text = "Dirty Camera Restarted";
-			myWrapper.StartCamera();
 		}
 	}
 
 	public float ScanDelay;
 
 	public IEnumerator Scan(){
-		myDebug.text = "Scan Began";
+		myWrapper.StartCamera();
 		isScanning = true;
 		bool hasFound = false;
 		while (myStartSearch.isOn && !hasFound) {
-			myDebug.text = "Scan Start Cycle";
-			myDebug.text = "Pause Camera";
 			myWrapper.PauseCamera ();
-			myDebug.text = "Scan";
 			Texture2D scan = myWrapper.GetPicture ();
-			myDebug.text = "Camera Restarted";
 			myWrapper.StartCamera();
-			myDebug.text = "Check Scan";
 
 			if(scan != null){
-				myDebug.text = "Analyze Scan";
 				try{
 					ZXing.Result scanResult = QrCodeWrapper.DecodeImage(scan);
-					myText.text = "Scan Not Null";
 					if(scanResult != null){
-						myText.text = "Scan Result Not Null";
 						string result = scanResult.Text;
 						if(result != null){
-							myText.text = "Result Not Null";
 							myContent = result;
 							hasFound = true;
 						}
 					}
 				}
 				catch(System.Exception e){
-					myDebug.text = e.Message;
-					//myText.text = e.Message;
+					Debug.LogException(e);
 				}
 			}
 			yield return new WaitForSeconds(ScanDelay);
 		}
-		myDebug.text = "Scan Ended";
-		myWrapper.StartCamera();
-		myDebug.text = "Camera Restarted";
+		myWrapper.StopCamera();
 		myStartSearch.isOn = false;
-		myDebug.text = "Disabling Search Button";
 		myText.text = myContent;
-		myDebug.text = "Disabling Scan";
+
+		if (IoWrapper.IsUrl (myContent)) {
+			yield return StartCoroutine (IoWrapper.AttemptAndroidUrl (myContent));//,myDebug));
+		}
 		isScanning = false;
 		yield return null;
 	}
-
 }
