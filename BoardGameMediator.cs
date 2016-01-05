aa8f0adf24ab832b7d48a0ac92caa3b90f9c0a5d
Marcus Kertesz <ModernMAK@Hotmail.com>
2016-01-05 09:45:35 -0800

    Update BoardGameMediator.cs

--- BoardGameMediator.cs
+++ BoardGameMediator.cs
@@ -2,7 +2,7 @@
 public static class BoardGameMediator{
 	//The old format is = Shelf #<A>, <B>, <C>, <D>, <E> - <F> players, <G> minutes, <H> to learn
 	public const string 
-	    QR_CODE_FORMAT_V1 = "Shelf #<Shelf>, <Name>, <Rating>, <Type>, <MinPlayers> - <MaxPlayers>, <Length> minutes, <Difficulty> to learn",
+	    QR_CODE_FORMAT_V1 = "Shelf #{0}, {1}, {2}, {3}, {4} - {5}, {6} minutes, {7} to learn",
 	    QR_CODE_FORMAT_V2; //Unfortunately, I don't have the new Format flushed out yet, it will either be the ID or a list of quialities similar to the last.
 
     //I hope to use a generic convert to method, but for now, hardcoding is best.
@@ -10,19 +10,9 @@ public static class BoardGameMediator{
     //Follows a similiar pattern to searching the database.
     public static string buildQrCodeV1(int shelf = -1, string name = null, string rating = null, string type = null, int minPlayers = -1, int minPlayers = -1, string length = null, string difficulty = null){
         String code = QR_CODE_FORMAT_V1;
+        //We could also do string format; which seems like a decent idea.
         //We could also just build the string backwards, by insertion, or append to the end of another string... think about it.
-        code.Replace("<Shelf>",shelf);
-        code.Replace("<Name>",name);
-        code.Replace("<Rating>",rating);
-        code.Replace("<Type>",type);
-        code.Replace("<MinPlayers>",minPlayers);
-        code.Replace("<MaxPlayers>",maxPlayers);
-        code.Replace("<Length>",length);
-        code.Replace("<Difficulty>",difficulty);
-        return code;
-    }
-    public static string stripQrCodeV1(string code){
-        code.
+        return String.Format(QR_CODE_FORMAT_V1,shelf,name,rating,type,minPlayers,maxPlayers,length,difficulty);
     }
 
 }
